827d15dd04bd0704e7ae8d89481e421bf705c0bf
Ashkan Khani <drashkankhani@gmail.com>
2024-08-12 20:05:01 +0330

    add files

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-print('ok')
-print('ok'
-print('ok')
-print('ok')
-print('ok')
+#print('test')
+
+#sinle line
+#این کد داره تست رو پرینت میگیره
+'این کد داره تست رو پرینت میگیره'
+"این کد داره تست رو پرینت میگیره"
+#==================
+
+#print('test')
+#multi line
+
+#1wefwef
+#2wefewfw
+#3wefwef
+#wefkweoipfj
+
+'''piefhpqejfeqw
+wefwefwefw
+ewefwefwefw
+wefwefwe'''
+
+
+"""piefhpqejfeqw
+wefwefwefw
+ewefwefwefw
+wefwefwe"""
+
+
