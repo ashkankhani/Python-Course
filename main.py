4a52ecee4bc63ea0105b051b0989d2a18cbdbcbc
Ashkan Khani <drashkankhani@gmail.com>
2024-08-13 21:41:50 +0330

    add files

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-#print('test')
+x = "hello"
+y = 10
+z = 10.5
 
-#sinle line
-#این کد داره تست رو پرینت میگیره
-'این کد داره تست رو پرینت میگیره'
-"این کد داره تست رو پرینت میگیره"
-#==================
-
-#print('test')
-#multi line
-
-#1wefwef
-#2wefewfw
-#3wefwef
-#wefkweoipfj
-
-'''piefhpqejfeqw
-wefwefwefw
-ewefwefwefw
-wefwefwe'''
-
-
-"""piefhpqejfeqw
-wefwefwefw
-ewefwefwefw
-wefwefwe"""
-
-
+print(x)
+print(y)
+z = 5
+print(z)
